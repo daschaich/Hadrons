b6f55b288c24b7ed2dfd64011a5e98ebc567c6af
chrisculver <chris.cb.culver@gmail.com>
2021-08-17 16:57:31 +0100

    updated for production runs on test configs

--- PiPi_App/main.cpp
+++ PiPi_App/main.cpp
@@ -31,8 +31,6 @@ int main(int argc, char *argv[])
   }
   parameterFileName = argv[1];
 
-
-
 // initialise Grid /////////////////////////////////////////////////////////
     Grid_init(&argc, &argv);
     HadronsLogError.Active(GridLogError.isActive());
@@ -53,21 +51,19 @@ int main(int argc, char *argv[])
       read(reader, "global", globalPar);
     }
 
+//      read(reader, "inverterMass", inverterMass)
+
 
     // global initialisation
     application.setPar(globalPar);
-    // create modules //////////////////////////////////////////////////////////
+    /// create modules //////////////////////////////////////////////////////////
 
-
-    std::string configFileName = "";
-    {
-      XmlReader reader(parameterFileName);
-      read(reader, "config", configFileName);
-    }
+    //for remaining parameters
+    XmlReader reader(parameterFileName);
 
     // gauge field
     MIO::LoadNersc::Par gaugePar;
-    gaugePar.file = configFileName;
+    read(reader, "configFilePrefix", gaugePar.file);
     application.createModule<MIO::LoadNersc>("gauge", gaugePar);
     std::string flavor = "l";
 
@@ -83,13 +79,13 @@ int main(int argc, char *argv[])
     application.createModule<MUtilities::GaugeSinglePrecisionCast>("stoutf", stoutfPar);
 
 
-    //action
+    //action // TODO: MAKE CORRECT
     MAction::ScaledDWF::Par actionPar;
     actionPar.gauge = "stout";
-    actionPar.Ls = 12;
-    actionPar.M5 = 1.8;
+    actionPar.Ls = 16;
+    actionPar.M5 = 1.0;
     actionPar.scale=2.0;
-    actionPar.mass = 0.01;
+    read(reader, "inverterMass", actionPar.mass);
     actionPar.boundary = "1 1 1 -1";
     application.createModule<MAction::ScaledDWF>("DWF_outer_" + flavor, actionPar);
 
@@ -101,40 +97,98 @@ int main(int argc, char *argv[])
     MSolver::MixedPrecisionRBPrecCG::Par solverPar;
     solverPar.innerAction = "DWF_inner_" + flavor;
     solverPar.outerAction = "DWF_outer_" + flavor;
-    solverPar.residual = 1.0e-8;
-    solverPar.maxInnerIteration = 10000;
-    solverPar.maxOuterIteration = 10;
+    read(reader, "solverResidual", solverPar.residual);
+    read(reader, "solverMaxInnerIter",solverPar.maxInnerIteration);
+    read(reader, "solverMaxOuterIter",solverPar.maxOuterIteration);
     application.createModule<MSolver::MixedPrecisionRBPrecCG>("CG_" + flavor, solverPar);
 
-    // source
-    MSource::Point::Par ptPar;
-    ptPar.position = "0 0 0 0";
-    application.createModule<MSource::Point>("pt", ptPar);
-
-    // Wall source
-    MSource::Wall::Par wallPar;
-    wallPar.tW = 0;
-    wallPar.mom = "0. 0. 0.";
-    application.createModule<MSource::Wall>("wall", wallPar);
-
-    //prop
-    MFermion::GaugeProp::Par quarkPar;
-    quarkPar.solver = "CG_" + flavor;
-    quarkPar.source = "pt";
-    application.createModule<MFermion::GaugeProp>("Qpt_" + flavor, quarkPar);
-
-    //wall prop
-    MFermion::GaugeProp::Par wallquarkPar;
-    wallquarkPar.solver = "CG_" + flavor;
-    wallquarkPar.source = "wall";
-    application.createModule<MFermion::GaugeProp>("Qwall_" + flavor, wallquarkPar);
-
-    // sink
+    /// sink
     MSink::Point::Par sinkPar;
     sinkPar.mom = "0 0 0";
     application.createModule<MSink::ScalarPoint>("sink", sinkPar);
 
+    // sink to spin-color matrix
+    MSink::Point::Par scSinkPar;
+    scSinkPar.mom = "0 0 0";
+    application.createModule<MSink::SCMatPoint>("scSink1", scSinkPar);
+    application.createModule<MSink::SCMatPoint>("scSink2", scSinkPar);
 
+    Application::ObjectId id;
+    if(!push(reader, "sources"))
+    {
+        HADRONS_ERROR(Parsing, "Cannot open node 'sources' in parameter file'"
+                               + parameterFileName + "'");
+    }
+    std::vector<std::string> sourceNames;
+    if(!push(reader, "source"))
+    {
+        HADRONS_ERROR(Parsing, "Cannot open node 'sources/source' in parameter file '"
+                            + parameterFileName + "'");
+    }
+    do
+    {
+        read(reader, "id", id);
+        application.createModule(id.name, id.type, reader);
+        sourceNames.push_back(id.name);
+    } while(reader.nextElement("source"));
+    pop(reader);
+    pop(reader);
+
+    for(const auto &source: sourceNames)
+    {
+        MFermion::GaugeProp::Par quarkPar;
+        quarkPar.solver = "CG_l";
+        quarkPar.source = source;
+        auto propName = "Q"+source+"_l";
+        application.createModule<MFermion::GaugeProp>(propName, quarkPar);
+
+        MContraction::Meson::Par mesPar;
+        mesPar.output   = "mesons/"+source+"_ll";
+        mesPar.q1       = propName;
+        mesPar.q2       = propName;
+        mesPar.gammas   = "all";
+        mesPar.sink     = "sink";
+        application.createModule<MContraction::Meson>("meson_"+source+"_ll", mesPar);
+
+        MContraction::PiPi::Par pipiWallPar;
+        pipiWallPar.output = "pipi/"+source+"_llll";
+        pipiWallPar.q1 = propName;
+        pipiWallPar.q2 = propName;
+        pipiWallPar.q3 = propName;
+        pipiWallPar.q4 = propName;
+        pipiWallPar.sink1 = "scSink1";
+        pipiWallPar.sink2 = "scSink2";
+        application.createModule<MContraction::PiPi>("pipi_"+source+"_llll", pipiWallPar);
+    }
+
+    // source
+    //MSource::Point::Par ptPar;
+    //ptPar.position = "0 0 0 0";                                 //TODO read from XML
+    //application.createModule<MSource::Point>("pt", ptPar);
+
+    // Wall source
+    //MSource::Wall::Par wallPar;
+    //wallPar.tW = 0;                                             //TODO read from XML
+    //wallPar.mom = "0. 0. 0.";
+    //application.createModule<MSource::Wall>("wall", wallPar);
+
+    /// read in option to create pt, wall, or both.
+    //prop
+    //MFermion::GaugeProp::Par quarkPar;
+    //quarkPar.solver = "CG_" + flavor;
+    //quarkPar.source = "pt";
+    //application.createModule<MFermion::GaugeProp>("Qpt_" + flavor, quarkPar);
+
+
+
+    //wall prop
+    //MFermion::GaugeProp::Par wallquarkPar;
+    //wallquarkPar.solver = "CG_" + flavor;
+    //wallquarkPar.source = "wall";
+    //application.createModule<MFermion::GaugeProp>("Qwall_" + flavor, wallquarkPar);
+
+/*
+    // use flag from above to switch on/off pt wall contractions
     // pion contraction
     MContraction::Meson::Par mesPar;
     mesPar.output   = "mesons/pt_ll";
@@ -143,14 +197,10 @@ int main(int argc, char *argv[])
     mesPar.gammas   = "all";
     mesPar.sink     = "sink";
     application.createModule<MContraction::Meson>("meson_pt_ll", mesPar);
+*/
 
 
-    // sink to spin-color matrix
-    MSink::Point::Par scSinkPar;
-    scSinkPar.mom = "0 0 0";
-    application.createModule<MSink::SCMatPoint>("scSink1", scSinkPar);
-    application.createModule<MSink::SCMatPoint>("scSink2", scSinkPar);
-
+/*
     // pi-pi contraction
     MContraction::PiPi::Par pipiPar;
     pipiPar.output = "pipi/pt_llll";
@@ -172,11 +222,7 @@ int main(int argc, char *argv[])
     pipiWallPar.sink1 = "scSink1";
     pipiWallPar.sink2 = "scSink2";
     application.createModule<MContraction::PiPi>("pipi_wall_llll", pipiWallPar);
-
-
-
-
-
+*/
 
 
     // execution ///////////////////////////////////////////////////////////////
